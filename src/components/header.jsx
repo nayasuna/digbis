124452574684753f2971a173f4d730ce04390ae7
nayasuna <nayasuna01@students.amikom.ac.id>
2024-07-04 10:18:35 +0700

    membuat landing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -26,7 +26,6 @@ const Header = () => {
             <li><a href="#features" className="text-white hover:text-yellow-400">Features</a></li>
             <li><a href="#contact" className="text-white hover:text-yellow-400">Contact</a></li>
             <li><button className="bg-transparent rounded-3xl border border-white text-white px-4 py-2">Login</button></li>
-            <li><button className="bg-transparent rounded-3xl border border-white text-white px-4 py-2">Daftar</button></li>
           </ul>
         </nav>
 
