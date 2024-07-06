0a801513620997bf052bc2187d76e26c3d1be353
nayasuna <nayasuna01@students.amikom.ac.id>
2024-07-06 08:23:46 +0700

    membuat landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import Footer from './components/footer';
-import AboutSection from './components/aboutus';
-import FeaturesSection from './components/feature';
-import Testimonials from './components/testimonials';
-import Header from './components/header';
-import HomeSection from './components/hero';
-import ContactUsMenu from './components/contatus';
-import Faq from './components/faqs';
+import Header from './pages/header';
+import HomeSection from './pages/hero';
+import AboutSection from './pages/aboutus';
+import Testimonial from './pages/testimonials';
+import FeatureSection from './pages/feature';
+import ContactUsMenu from './pages/contactus';
+import Footer from './pages/footer';
+import Faq from './pages/faqs';
+
 
 
 function App() {
   return (
     <>
-      <Header />
-      <HomeSection/>
-      <AboutSection/> 
-      <Testimonials />
-      <FeaturesSection />
+      <Header></Header>
+      <HomeSection></HomeSection>
+      <AboutSection></AboutSection>
+      <Testimonial></Testimonial>
+      <FeatureSection></FeatureSection>
       <Faq></Faq>
       <ContactUsMenu></ContactUsMenu>
-      <Footer />
-      
+      <Footer></Footer>
     </>
   )
 }
